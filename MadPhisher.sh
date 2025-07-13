0ea764f2065a90e3a1d476d5bd30b49186494848
dashmeet2023 <148901387+dashmeet2023@users.noreply.github.com>
2025-07-14 01:41:31 +0530

    MadPhisher.sh

--- MadPhisher.sh
+++ MadPhisher.sh
@@ -4,7 +4,7 @@
 ##   MadPhisher : 	Automated Phishing Tool
 ##   Author 	: 	Dashmeet Singh
 ##   Version 	: 	2.1
-##   Github 	: 	https://github.com/SajidIbnNayeem
+##   Github 	: 	https://github.com/dashmeet2023
 
 
 __version__="2.1"
